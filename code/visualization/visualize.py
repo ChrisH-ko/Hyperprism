f3918713e1f9d5df031d8663265a2d9d7d508105
ChrisHReger <csphoes@gmail.com>
2024-01-17 17:37:14 +0100

    more visualizer

--- code/visualization/visualize.py
+++ code/visualization/visualize.py
@@ -5,14 +5,18 @@ def visualize(chip):
     gates_x = [chip.gates[i].position[0] for i in chip.gates]
     gates_y = [chip.gates[i].position[1] for i in chip.gates]
 
-    _ = plt.scatter(gates_x, gates_y)
-    _ = plt.xlim(0, max(gates_x)+1)
-    _ = plt.ylim(0, max(gates_y)+1)
+    plt.scatter(gates_x, gates_y)
+    plt.xlim(0, max(gates_x)+1.05)
+    plt.ylim(-0.05, max(gates_y)+1)
 
 
-    _ = plt.box(False)
-    _ = plt.tick_params(length=0, width=0, labelsize=0)
-    _ = plt.grid()
+    plt.box(False)
+    plt.tick_params(length=0, width=0, labelsize=0)
+    plt.grid()
 
-    _ = plt.show()
-    return gates_x, gates_y
+    ax = plt.gca()
+    ax.set_aspect("equal")
+    ax.set_xticks([i for i in range(0, max(gates_x)+2)])
+    ax.set_yticks([i for i in range(0, max(gates_y)+2)])
+    ax.set_axisbelow(True)
+    plt.show()
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from code.classes import chip
 from code.visualization import visualize as vis
 
 if __name__ == "__main__":
-    chip_id = 0
-    net_id = 1
+    chip_id = 2
+    net_id = 7
 
     chip_file = f'gates&netlists/chip_{chip_id}/print_{chip_id}.csv'
     netlist = f'gates&netlists/chip_{chip_id}/netlist_{net_id}.csv'
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     test_chip = chip.Chip(chip_id, chip_file, net_id, netlist)
 
     print(test_chip)
-    print(vis.visualize(test_chip))
+    vis.visualize(test_chip)
