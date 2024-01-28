996ef5443ffa4aa5e649fa7f2029815fd5973b20
ChrisHReger <csphoes@gmail.com>
2024-01-28 20:07:37 +0100

    Can save a reconnection or discard it if the cost does not improve

--- code/algorithms/netlist_solver/net_solver.py
+++ code/algorithms/netlist_solver/net_solver.py
@@ -4,6 +4,8 @@ import tqdm
 from code.algorithms.path_solver.standard_astar_alg import Standard_pathwise_astar
 from code.algorithms.path_solver.make_space_astar import Make_Space
 
+from code.visualization import visualize as vis
+
 pathfinders = [Standard_pathwise_astar, Make_Space]
 
 class Net_Solver():
@@ -30,16 +32,33 @@ class Net_Solver():
             nets = self.nets
 
         for net in nets:
-            path = self.model.paths[net]
+            old_path = self.model.paths[net]
 
-            if path.complete():
-                path = path.blank_copy_path()
-                self.model.remove_path(net)
+            path = old_path.blank_copy_path()
+            self.model.remove_path(net)
 
             solver = self.pathfinder(self.model, path)
             solver.run()
             new_path = solver.solution
-            self.model.add_path(net, new_path)
+
+            if old_path.complete():
+                self.compare_paths(old_path, new_path)
+            else:
+                self.model.add_path(net, new_path)
+    
+    def compare_paths(self, old_path, new_path):
+        net = old_path.connection.id
+
+        if new_path.complete():
+            old_cost = self.model.path_cost(old_path)
+            new_cost = self.model.path_cost(new_path)
+
+            if new_cost < old_cost:
+                self.model.add_path(net, new_path)
+                return
+        
+        self.model.add_path(net, old_path)
+
 
 class Random_Net_Order(Net_Solver):
     def get_net_order(self):
