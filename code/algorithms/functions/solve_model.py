0f741089bff782b46c11bb11db4a09ccbc635bb5
ChrisHReger <csphoes@gmail.com>
2024-01-31 18:52:59 +0100

    Solve model based on user input

--- code/algorithms/functions/solve_model.py
+++ code/algorithms/functions/solve_model.py
@@ -20,7 +20,11 @@ pathfinders = {
 
 text_bar = '-'*40
 
+
 def start_solver(model, ns, pf, from_scratch):
+    """
+    Run a netsolver with an accompanying pathfinder on the give model.
+    """
     print('\n')
     print(text_bar + ns)
     print(text_bar + pf)
@@ -34,7 +38,11 @@ def start_solver(model, ns, pf, from_scratch):
 
     return solver
 
+
 def display_solution(solution, history):
+    """
+    Display the provided solution upon user request.
+    """
     visualize = None
     print(f"Show solution? \n{['y', 'n']}")
     while visualize not in ['y', 'n']:
@@ -43,9 +51,14 @@ def display_solution(solution, history):
         if visualize == 'y':
             vis.visualize(solution.model, history)
 
+
 def check_existing_solution(model, solution, history):
+    """
+    Check whether a solution has already been made and whether to override it.
+    """
     if solution is not None:
         print('\n \n')
+        print('Solution has already been found: ')
         solution.results()
 
         display_solution(solution, history)
@@ -55,6 +68,7 @@ def check_existing_solution(model, solution, history):
         while answer not in ['y', 'n']:
             answer = input(': ')
         
+        # Returns the solved model if yes, clears history if no
         if answer == 'y':
             return solution.model, history
         if answer == 'n':
@@ -63,29 +77,49 @@ def check_existing_solution(model, solution, history):
     
     return model, history
 
+
+def get_algorithm(type):
+    """
+    Asks the user which algorithm should be used from the type that has been provided.
+    """
+    if type == 'net':
+        name, choices = 'netsolver', list(netsolvers.keys())
+    elif type == 'path':
+        name, choices = 'pathfinder', list(pathfinders.keys())
+
+    algorithm = None
+    print(f'Which {name}? \n{choices}')
+    while algorithm not in choices:
+        algorithm = input(': ')
+    
+    return algorithm
+
+
 def solve_model(model, solution, history):
+    """
+    Starts the model solver after selecting a netsolver and pathfinder.
+    """
+    # Check if solution already exists.
     model, history = check_existing_solution(model, solution, history)
 
     print('\n \n')
-
-    netsolver = None
-    print(f'Which netsolver? \n{list(netsolvers.keys())}')
-    while netsolver not in list(netsolvers.keys()):
-        netsolver = input(': ')
     
-    pathfinder = None
-    print(f'Which pathfinder? \n{list(pathfinders.keys())}')
-    while pathfinder not in list(pathfinders.keys()):
-        pathfinder = input(': ')
+    # Get netsolver and pathfinder
+    netsolver = get_algorithm('net')
+    pathfinder = get_algorithm('path')
     
+    # Check if we are rewiring an existing solution
     from_scratch = True
     if len(history) > 0:
         from_scratch = False
     
+    # Start solving the model
     solver = start_solver(model, netsolver, pathfinder, from_scratch)
 
+    # Add the algorithms used to the history
     history.append(f'{netsolver}+{pathfinder}')
 
+    # Display the solution
     display_solution(solver, history)
 
     return solver, history
