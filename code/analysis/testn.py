7a53523dbb2dbfc0cbfa028b3d13abc9773ed176
ChrisHReger <csphoes@gmail.com>
2024-01-31 22:54:06 +0100

    some documentation

--- code/analysis/testn.py
+++ code/analysis/testn.py
@@ -8,21 +8,25 @@ def run_n(model, algorithm, iterations, save=True):
     completions = []
 
     best_model = None
-    lowest_cost = 100000
+    lowest_cost = 1000000
 
+    # Write costs and completions to a file.
     if save:
-        file = open(f"outputs/test{iterations}.txt", 'a')
+        file = open(f"outputs/test_{algorithm}.txt", 'a')
 
     for _ in tqdm.tqdm(range(iterations)):
+        # Run solver
         solution = algorithm(model)
         solution.run(verbose=False)
 
+        # Record results
         cost = solution.cost()
         completion = solution.completion()
 
         costs.append(cost)
         completions.append(completion)
 
+        # Compare to best model found
         if cost < lowest_cost and completion == 1:
             best_model = solution
             lowest_cost = cost
